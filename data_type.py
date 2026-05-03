59e493fd00d6aa291162d4d3c1d64fe5ff67d1a7
sumeet <sumeet@gmail.com>
2026-05-03 22:58:12 +0530

    data type in python

---
+++ data_type.py
@@ -0,0 +1,33 @@
+# All type of data type in pythone
+a = 10
+b = 10.0
+c = "hello"
+d = "True"
+e = [1,2,3]
+f = (1,2,3)
+g = {"a":1}
+h = {1,2,3}
+i = range(4)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+
+""""
+Output of above is 
+<class 'int'>
+<class 'float'>
+<class 'str'>
+<class 'str'>
+<class 'list'>
+<class 'tuple'>
+<class 'dict'>
+<class 'set'>
+<class 'range'>
+"""
