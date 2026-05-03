8874973364cf41d0c0051a57a560740885bef07e
sumeet <sumeet@gmail.com>
2026-05-03 23:46:51 +0530

    identity operators will check data same same memory

---
+++ identity_operators.py
@@ -0,0 +1,26 @@
+# This will check value save at same memroy location or not
+
+a = 10
+b = 10
+
+print(a is b)
+
+# output is True
+
+x = [1,2,3]
+
+y = [1,2,3]
+
+print(x is y)
+
+# output is False
+
+print(x is not  y)
+# output is True
+
+h = 256
+k = 256
+
+print(h is k)
+
+# output is True
