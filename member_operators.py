9f24d878c6e11f1a208d7501bce763986f393b90
sumeet <sumeet@gmail.com>
2026-05-03 23:37:08 +0530

    member operators in python

---
+++ member_operators.py
@@ -0,0 +1,19 @@
+# it will check the value is presnt or not in list
+
+fruits = ["banana","apple","orange"]
+
+print( "apple" in fruits)
+
+# output is True
+
+print("Apple" in fruits)
+
+# output is False
+
+print("Apple" not in fruits)
+
+# output is True
+
+print( "apple" not  in fruits)
+
+# output is False
