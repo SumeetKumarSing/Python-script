a6a9e5fc254b8c1b021225313edb8b348c3e6186
sumeet <sumeet@gmail.com>
2026-05-03 23:28:03 +0530

    assign operators in python

---
+++ assign_operators.py
@@ -0,0 +1,29 @@
+# In This we will check the assign operators 
+
+a = 5
+print(a)
+
+y = 10
+y += 5
+print(y)
+
+z = 10
+z -= 5
+print(z)
+
+a = 10
+a *= 5
+print(a)
+
+b = 10
+b /= 2
+print(b)
+
+"""
+Output is above 
+5
+15
+5
+50
+5.0
+"""
