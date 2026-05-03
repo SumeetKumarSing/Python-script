8df047acf27ce03942237c32f9955544978f6cb6
sumeet <sumeet@gmail.com>
2026-05-03 23:04:01 +0530

    data type changes script

---
+++ casting.py
@@ -0,0 +1,31 @@
+# We are changing data types 
+
+a = "10"
+print(type(a))
+# changing string to int
+a = int(a)
+print(type(a))
+"""
+Output of above is
+<class 'str'>
+<class 'int'>
+"""
+b = 10.0
+print(type(b))
+# changing float to int
+b = int(b)
+print(type(b))
+"""Output of above is
+<class 'float'>
+<class 'int'>
+"""
+
+c = 10
+print(type(c))
+# changing int to float
+c = float(c)
+print(type(c))
+"""Output of above is
+<class 'int'>
+<class 'float'>
+"""
