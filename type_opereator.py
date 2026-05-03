16652363fcb4681fbb89f3d3aad27b6ec76a927d
sumeet <sumeet@gmail.com>
2026-05-03 23:13:07 +0530

    maths operators in python

---
+++ type_opereator.py
@@ -0,0 +1,21 @@
+# maths operators in python
+
+a = 20
+b = 10
+print(a + b)  # addition
+print(a - b)  # subtraction
+print(a * b)  # multiplication
+print(a / b)  # division
+print(a ** b)  # exponentiation
+print(7 // 2)  # floor division
+print(a % b)  # modulus
+"""
+Output of above is
+30 # addition
+10 # subtraction
+200 # multiplication
+2.0 # division
+10240000000000 # exponentiation
+3 # floor division
+0 # modulus
+"""
