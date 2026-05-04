93eaa332855b8b2aebfece4d175d5ca60849e9c9
sumeet <sumeet@gmail.com>
2026-05-04 23:07:51 +0530

    Condition use in python

---
+++ condition.py
@@ -0,0 +1,11 @@
+a = int(input("Type the marks: "))
+
+if a >= 90:
+    print("You obtain A grade")
+elif a >= 80:
+    print("You obtain B grade")
+
+elif a >= 60:
+    print("You obtain C grade")
+else:
+    print("FAIL")
