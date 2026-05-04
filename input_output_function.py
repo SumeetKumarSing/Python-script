e79ec9031b1d9920f139dbc71ae39f8996022083
sumeet <sumeet@gmail.com>
2026-05-04 22:58:29 +0530

    Input and output funcation in python

---
+++ input_output_function.py
@@ -0,0 +1,32 @@
+# Types of function 
+
+a = input("Type the no ")
+
+print(a)
+print(type(a))
+
+""" output is 
+Type the no 10
+10
+<class 'str'>
+"""
+
+b = int(input("Type the no "))
+
+print(b)
+print(type(b))
+
+"""
+output is 
+a = input("Type the no ")
+
+print(a)
+print(type(a))
+"""
+
+"""
+input is input function
+print is output function 
+
+input always take as string text
+"""
